b2ee72a0b16e4eae5ca677b99af7a7487643beb7
Rishabh Sharma <rishabh78974@gmail.com>
2021-07-04 16:43:53 +0530

    created repo interface

---
+++ src/main/java/com/Rapid/kenApp/repository/EmployeeRepo.java
@@ -0,0 +1,25 @@
+package com.Rapid.kenApp.repository;
+
+import com.example.kenassignment.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+    @Query(value="select * from employees_list where department = :depname ",nativeQuery = true)
+    List<Employee> findByDepartment(@Param("depname") String department);
+
+    @Query(value="select * from employees_list where position = :position ",nativeQuery = true)
+    List<Employee> findByPosition(@Param("position") String position);
+
+    @Query(value="select * from employees_list limit  :limit ",nativeQuery = true)
+    List<Employee> findByLimit(@Param("limit") int limit);
+
+    @Query(value="select * from employees_list where position=:position ,department=:department limit  :limit ",nativeQuery = true)
+    List<Employee> findByDepartmentPositionLimit(@Param("position")String position ,@Param("department")String department, @Param("limit") int limit);
+
+}
