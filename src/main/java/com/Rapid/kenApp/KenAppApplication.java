7022dddeca52271fcda2e0745df62b0aeca45351
Rishabh Sharma <rishabh78974@gmail.com>
2021-07-04 16:35:16 +0530

    starting backend

---
+++ src/main/java/com/Rapid/kenApp/KenAppApplication.java
@@ -0,0 +1,13 @@
+package com.Rapid.kenApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Rapid/kenApp/KenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rapid.kenApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
